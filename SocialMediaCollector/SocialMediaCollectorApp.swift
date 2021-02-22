f5a8db6514db5aa9f05ac29808dea80e77240659
Evan Coulson <evan.m.coulson@gmail.com>
2021-02-22 11:44:33 -0700

    Initial Commit

---
+++ SocialMediaCollector/SocialMediaCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaCollectorApp.swift
+//  SocialMediaCollector
+//
+//  Created by Evan Coulson on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
